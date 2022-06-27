e53ebee785130f1e9d7127886d0278a102d82f6b
Mateusz <oureyelet@gmail.com>
2022-06-27 19:15:33 +0100

    iostream intoroduction

--- 23.1 — Input and output (I/O) streams (vsCode)/main.cpp
+++ 23.1 — Input and output (I/O) streams (vsCode)/main.cpp
@@ -5,5 +5,13 @@ int main()
     std::cout << "Hello World" << '\n';
     // :)
 
+    //The iostream library:
+
+    //Streams:
+
+    //Input/output in C++:
+
+    //Standard streams in C++:
+
     return 0;
 }
